dbc62d8eb662389ff8ff266f4c653052c24d9848
Jarling Duque <jarlingduqueduarte@gmail.com>
2022-03-30 15:24:25 -0500

    backend

---
+++ server.js
@@ -0,0 +1,11 @@
+const jsonserver = require('json-server');
+const server = jsonserver.create();
+const router = jsonserver.router('backendveronicaduque.json');
+const middlewares = jsonserver.defaults();
+
+server.use(middlewares);
+server.use(router);
+const port = process.env.PORT || 4000;
+server.listen(port, () => {
+    console.log('JSON Server is running');
+})
